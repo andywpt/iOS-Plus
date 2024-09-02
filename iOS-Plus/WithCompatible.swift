df7fce41090d4bb78d4c89873b6c705bd3631d50
Andy Wu <andywu.england@gmail.com>
2024-09-02 10:21:18 +0800

    Add WithCompatible

---
+++ iOS-Plus/WithCompatible.swift
@@ -0,0 +1,18 @@
+import CoreGraphics
+
+// https://gist.github.com/nicklockwood/9b4aac87e7f88c80e932ba3c843252df
+// https://blog.eppz.eu/declarative-uikit-with-10-lines-of-code
+protocol WithCompatible {}
+
+extension WithCompatible where Self: AnyObject {
+    @discardableResult
+    func with(_ configuration: (inout Self) -> Void) -> Self {
+        var copy = self
+        configuration(&copy)
+        return copy
+    }
+}
+
+extension NSObject: WithCompatible {}
+extension CGPath: WithCompatible {}
+extension CGContext: WithCompatible {}
