1c7f461aded09e1d6f355fbfd1684fa14de28ffe
Andy <andywu.england@gmail.com>
2024-09-02 11:06:18 +0800

    Add print.swift

---
+++ iOS-Plus/print.swift
@@ -0,0 +1,8 @@
+func print(_ item: Any, file: String = #file, line: Int = #line){
+    guard Environment.isDebugBuild else { return }
+    let lastSlashIndex = (file.lastIndex(of: "/") ?? String.Index(utf16Offset: 0, in: file))
+    let nextIndex = file.index(after: lastSlashIndex)
+    let filename = file.suffix(from: nextIndex)
+    let prefix = "[\(filename):\(line)]"
+    Swift.print("\(item) \(prefix)[Debug]")
+}
